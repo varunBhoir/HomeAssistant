2c27138f6fa92b69d846a199484656410a5d8016
bhoirvarun6@gmail.com <bhoirvarun6@gmail.com>
2021-03-24 16:39:15 +0530

    Initial Commit

---
+++ HomeAssistant/HomeAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAssistantApp.swift
+//  HomeAssistant
+//
+//  Created by varun bhoir on 24/03/21.
+//
+
+import SwiftUI
+
+@main
+struct HomeAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
